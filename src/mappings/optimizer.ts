6d4aa9ae38db318f7803f4216c981bb97bd17cad
Alex <tarkovskiy.alexandr@protonmail.com>
2023-03-26 18:16:58 +0300

    remove analytic

--- src/mappings/optimizer.ts
+++ src/mappings/optimizer.ts
@@ -1,10 +1,7 @@
-import { dataSource, Address } from '@graphprotocol/graph-ts';
 import { Deposit, CollectFees, Withdraw } from '../../generated/templates/UniswapV3Pool/PopsicleV3Optimizer';
-import { PopsicleAnalytics } from '../../generated/templates/UniswapV3Pool/PopsicleAnalytics';
 import { EventType, GLOBAL_DIVISIONER, PROTOCOL_FEE } from '../constants';
 import { getOrCreateOptimizer } from '../helpers/optimizer';
 import { updateAccountState, updateFees } from '../helpers/updates';
-import { getAnalyticAddress } from '../helpers/get-analytic-address';
 
 export function handleLogDeposit(event: Deposit): void {
     const optimizerAddress = event.address;
@@ -21,26 +18,12 @@ export function handleLogWithdraw(event: Withdraw): void {
 }
 
 export function handleLogCollectFees(event: CollectFees): void {
-    const analyticAddress = getAnalyticAddress(dataSource.network());
     const optimizerAddress = event.address;
     const optimizer = getOrCreateOptimizer(optimizerAddress);
     if (!optimizer) return;
 
-    let feesFromPool0 = event.params.feesFromPool0;
-    let feesFromPool1 = event.params.feesFromPool1;
-
-    const analytic = PopsicleAnalytics.bind(Address.fromString(analyticAddress));
-    const burnZeroLiquidityCall = analytic.try_burnZeroLiquidity(Address.fromString(optimizer.id));
-    if (!burnZeroLiquidityCall.reverted && optimizer.lastCollectBlock != event.block.number) {
-        feesFromPool0 = feesFromPool0.plus(burnZeroLiquidityCall.value.getValue0());
-        feesFromPool1 = feesFromPool1.plus(burnZeroLiquidityCall.value.getValue1());
-
-        optimizer.lastCollectBlock = event.block.number;
-        optimizer.save();
-    }
-
-    feesFromPool0 = feesFromPool0.minus(feesFromPool0.times(PROTOCOL_FEE).div(GLOBAL_DIVISIONER));
-    feesFromPool1 = feesFromPool1.minus(feesFromPool1.times(PROTOCOL_FEE).div(GLOBAL_DIVISIONER));
+    const feesFromPool0 = event.params.feesFromPool0.minus(event.params.feesFromPool0.times(PROTOCOL_FEE).div(GLOBAL_DIVISIONER));
+    const feesFromPool1 = event.params.feesFromPool1.minus(event.params.feesFromPool1.times(PROTOCOL_FEE).div(GLOBAL_DIVISIONER));
 
     updateFees(optimizer, [feesFromPool0, feesFromPool1], event.block);
 }
